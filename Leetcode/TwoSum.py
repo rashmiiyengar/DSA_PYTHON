c462e406e9b44ec0e5c5e44ab84dd82958ea3ba2
rashmiiyengar <rashmiiyengar255@gmail.com>
2024-10-28 21:06:39 -0700

    two sum

---
+++ Leetcode/TwoSum.py
@@ -0,0 +1,17 @@
+from typing import List
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+       dict={}
+       for i in range(len(nums)):
+            diff = target-nums[i]
+            if(diff in dict):
+                return [dict[diff],i]
+            dict[nums[i]]=i
+
+
+solution = Solution()
+
+nums =[2,7,11,15]
+list =solution.twoSum(nums,9)
+print(list)
