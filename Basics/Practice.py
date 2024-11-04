3bbebe79a7d9d9a3855caf26b6b874f8ffe0bb97
rashmiiyengar <rashmiiyengar255@gmail.com>
2024-11-03 18:46:16 -0800

    14. Longest Common Prefix

--- Basics/Practice.py
+++ Basics/Practice.py
@@ -1,3 +1,23 @@
-a=abs(2)
+from typing import List
 
-print(a)
+class Solution:
+    def longestCommanPrefix(self,strs:List[str])->str:
+        min_length= float('inf')
+        
+        for s in strs:
+            if len(s) < min_length:
+                min_length = len(s)
+    
+        i=0
+        while i < min_length:
+            for s in strs:
+                if s[i]!= strs[0][i]:
+                    return s[:i]
+            i+=1
+        
+        return s[:i]
+            
+                    
+solution = Solution()
+commanPrefix=solution.longestCommanPrefix(["flower","flow","flight"])
+print(commanPrefix)
