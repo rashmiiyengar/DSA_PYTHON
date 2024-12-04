13d717a7c8820858ca8b146b96e72828d9c62170
rashmiiyengar <rashmiiyengar255@gmail.com>
2024-12-03 21:51:06 -0800

    Move zeros to end of array 283

---
+++ Leetcode/MoveZero_283.py
@@ -0,0 +1,20 @@
+from typing import List
+
+class Solution:
+    def moveZeroes(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        l=0
+
+        for r in range(len(nums)):
+            if nums[r]:
+                nums[l],nums[r] = nums[r], nums[l]
+                l+=1
+        return nums
+    
+solution = Solution()
+
+nums = [0,1,5,0,12]
+value=solution.moveZeroes(nums)
+print(value)
