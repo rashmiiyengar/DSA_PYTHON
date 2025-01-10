8a5cc0deb00ca7ced924e13c38148d8130a64a9d
rashmiiyengar <rashmiiyengar255@gmail.com>
2025-01-10 13:17:58 -0800

    ivert binary tree

--- Leetcode/Grind-75/BinarySearch_704.py
+++ Leetcode/Grind-75/BinarySearch_704.py
@@ -1,5 +1,12 @@
 from typing import List
 
+#Given an array of integers nums which is sorted 
+#in ascending order, and an integer target, 
+#Write a function to search target in nums.
+#If target exists, then return its index. Otherwise, return -1.
+
+#You must write an algorithm with O(log n) runtime complexity.
+
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
         left =0
