f4cc4359c0837e5c559b499d645722fa3064dd8a
rashmiiyengar <rashmiiyengar255@gmail.com>
2025-01-23 11:39:09 -0800

    armstrong numnber

--- Leetcode/Interviews/ArmstrongNumber.py
+++ Leetcode/Interviews/ArmstrongNumber.py
@@ -3,6 +3,13 @@ def armstrongNumber(num):
     num_str=str(num)
     len_str=len(num_str)
     
+    total= sum(int(digit) ** len_str for digit in num_str)
+    
+    if total == num:
+        print(f"{num} is armstrongNumber number")
+    else:
+        print(f"{num} is not an armstrong number")
+
 
 
 num=153
