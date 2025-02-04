282467927e9e0ff97ab5f11c565a1452429086b8
rashmiiyengar <rashmiiyengar255@gmail.com>
2025-02-04 12:48:56 -0800

    remove duplicates

---
+++ Leetcode/Interviews/RemoveDuplicates.py
@@ -0,0 +1,19 @@
+#Remove duplicates in place from a sorted array
+#1,2,3,3,4,4,5
+#1,2,3,4,5,_,_
+#return number of unique elements
+
+def removeDuplicates(arr):
+    
+    j=0 # consider j pointer
+    
+    for i in range(1,len(arr)):
+        if arr[i]!=arr[j]:
+            j+=1
+        arr[j]=arr[i]
+    
+    return j+1
+            
+      
+
+print(removeDuplicates([1,2,2,3,4,4,4,5]))
