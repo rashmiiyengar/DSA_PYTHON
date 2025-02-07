425a65908904a926063df5ac23fc6cdd92369c8d
rashmiiyengar <rashmiiyengar255@gmail.com>
2025-02-07 12:25:49 -0800

    buy and sell stock

--- Leetcode/Grind-75/Buyandsellstock_121.py
+++ Leetcode/Grind-75/Buyandsellstock_121.py
@@ -11,21 +11,21 @@ from typing import List
 
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        max_profit= 0
-        min_price= float('inf')
+        max_profit=0
+        min_price=float("inf")
 
-        for price in prices:
-            if price < min_price:
+        for  price in prices:
+            if price<min_price:
                 min_price=price
             
-            profit = price-min_price
-
+            profit=price-min_price
+            
             if profit>max_profit:
                 max_profit=profit
-
+        
         return max_profit
 
-
-
+solution= Solution()
+print(solution.maxProfit([7,1,5,3,6,4]))
 
 
