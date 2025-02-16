b0067dbeac4d2aa76554c8e8daa02a441c9fba4e
rashmiiyengar <rashmiiyengar255@gmail.com>
2025-02-16 13:32:14 -0800

    linear search

--- Leetcode/Grind-75/InvertBinaryTree_226.py
+++ Leetcode/Grind-75/InvertBinaryTree_226.py
@@ -14,6 +14,16 @@ class Solution:
         self.invertTree(root.left)
         self.invertTree(root.right)
         return root
-    
+
+root = TreeNode(4)
+root.left = TreeNode(2)
+root.right = TreeNode(7)
+root.left.left = TreeNode(1)
+root.left.right = TreeNode(3)
+root.right.left = TreeNode(6)
+root.right.right = TreeNode(9)
+
 solution=Solution()
-solution.invertTree(2)
+print(solution.invertTree(root))
+
+
---
+++ Leetcode/Interviews/LinearSearch.py
@@ -0,0 +1,8 @@
+def linearSearch(nums,num):
+    for i in range(len(nums)):
+        if nums[i]==num:
+            return i
+    return -1
+num=9
+ele=linearSearch([3,2,8,1,9,5],num)
+print(f"number {num} is at {ele}th index")
